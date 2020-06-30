22a046b2bef7ba103f0d3263531098e759ac701b
LYSANDER-PC\Lysander <finblack20@gmai.com>
2020-06-30 18:59:37 -0400

    fix bootshrap problem

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Contanct from './Componets/Contact';
 import Project from './Componets/Projects'
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 import 'bootstrap/dist/css/bootstrap.min.css'
